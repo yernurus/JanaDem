fb52e51af01849e93375a47036ac3d768d033bd9
yernurus <zhakullayernur@gmail.com>
2024-06-12 22:55:38 +0500

    last fixes

--- market/serializers/market.py
+++ market/serializers/market.py
@@ -29,3 +29,18 @@ class MarketItemOrderSerializer(serializers.ModelSerializer):
 class CreateMarketItemOrderSerializer(serializers.Serializer):
     item_id = serializers.IntegerField()
     quantity = serializers.IntegerField()
+
+
+class MyOrdersItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MarketItem
+        fields = ['name', 'price', 'image']
+
+
+class MyOrdersSerializer(serializers.ModelSerializer):
+    item = MyOrdersItemSerializer(read_only=True)
+    created_at = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S')
+
+    class Meta:
+        model = MarketItemOrder
+        fields = ['item', 'quantity', 'created_at']
